c2c46af36d5835c50655d65a466d654225498288
Nitesh Khare <niteskak25@gmail.com>
2024-08-25 15:21:40 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+   
+    </div>
+  );
+};
+
+export default Footer;
